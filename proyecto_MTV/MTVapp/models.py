9cdcba1a502c20e847f2c6d64f0bd449b18c98ed
gisele yani <yiyi.clc@gmail.com>
2022-08-06 14:41:09 -0300

    modificacion

--- proyecto_MTV/MTVapp/models.py
+++ proyecto_MTV/MTVapp/models.py
@@ -10,3 +10,28 @@ class Familia(models.Model):
     dni= models.IntegerField()
     domicilio= models.IntegerField()
     
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length =30)
+    email = models.EmailField()
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField (max_length=30)
+    
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)
+    echa_entrega = models.DateField()
+    entregado = models.BooleanField()
+    
+    
+
+
